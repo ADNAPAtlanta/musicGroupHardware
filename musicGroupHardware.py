6e30e22802dc1b2ce38085d9c26c0cf487d220a6
chris <cfarley@gmail.com>
2016-12-01 16:44:04 -0500

    0.1

--- musicGroupHardware.py
+++ musicGroupHardware.py
@@ -20,6 +20,7 @@ import shelve
 internal = 0
 spotify = 0
 itunes = 0
+menu = 0
 musicFolder = "/home/pi/Music"
 Firebase = firebase.FirebaseApplication("https://musicgroup-99932.firebaseio.com/")
 
@@ -99,15 +100,13 @@ class preferences(object):
     def __init__(self,voteTimeSetting,voteCycleSetting):
         self.voteTimeSetting = voteTimeSetting
         self.voteCycleSetting = voteCycleSetting
-    def setVoteTimeSetting(self,time):
-        voteTime = shelve.open("options.odb")
-        voteTime.write("Vote Time: %s" % time)
-        self.voteTimeSetting = time
+    def setVoteTimeSetting(self,voteTime):
+        voteTime = shelve.open("options")
+        self.voteTimeSetting = voteTime
         voteTime["voteTime"] = str(self.voteTimeSetting)
         voteTime.close()
     def setVoteCycleSetting(self,cycle):
-        voteCycle = shelve.open("options.odb")
-        voteCycle.write("Vote Cycle: %s" % cycle)
+        voteCycle = shelve.open("options")
         self.voteCycleSetting = cycle
         voteCycle["voteCycle"] = str(self.voteCycleSetting)
     def getVoteTimeSetting(self):
@@ -167,33 +166,70 @@ class internal(object):
             Firebase.patch("/Rooms/00001/" + artist + "-" + songName,{"votes":votes})
     def continueWatching(self):
         print("continue")
+        songQueue = []
+        db = firebasePyre.database()
+        my_stream = db.child("Rooms").child("00001").stream(stream_handler)
+        time.sleep(duration)
+        songsByScore = db.child("Rooms").child("00001").order_by_child("votes").limit_to_first(10).get()
+        for song in songsByScore.each():
+            print(song)
+            
+        print(songsByScore)
 
+def mainMenu():
+    mode = input("What is the mode?\n 1.internal update\n 2.internal\n 3.preferences\n")
 
-getSetting = shelve.open("options.odb")
+    if mode == "Spotify":
+        menu = 0
+        print("test")
+        #spotify work
+    elif mode == "internal update":
+        #print(internal())
+        playNextSong = input("Play nexr song? (y/n)")
+        if playNextSong == "y":
+            print(playSong())
+            print(continueWatching(songDuration))
+    elif mode == "preferences":
+        getSetting = shelve.open("options")
+        cycle = int(input("how often is the voting cycle"))
+        settings.setVoteCycleSetting(cycle)
+        getSetting["voteCycle"] = cycle
+        time = int(input("How much time is availible to vote?"))
+        settings.setVoteTimeSetting(Time)
+        getSetting["voteTime"] = Time
+        
+        mainMenu()
+    
+
+    elif mode == "internal":
+        mode = internal(settings.getVoteTimeSetting(),settings.getVoteCycleSetting())
+        playNextSong = input("1.update\n 2.start voting\n")
+        if playNextSong == "2" or "start voting":
+            print(settings.voteTimeSetting)
+            mode.startVoting()
+            time.sleep(settings.getVoteTimeSetting)
+            print(playSong())
+            print(continueWatching(songDuration))
+        elif playNextSong == "1" or "update":
+            mode.updateInternal()
+            playSong()
+            continueWatching(songDuration)
+    elif mode == "debug":
+        debugOption = input("1.")
+    else:
+        print("Not a valid option")
+        mainMenu()
+    
+getSetting = shelve.open("options")
 settings = preferences(getSetting["voteTime"],getSetting["voteCycle"])
 print(settings.getVoteCycleSetting(),settings.getVoteTimeSetting())
+mainMenu()
 
-mode = input("What is the mode?\n 1.internal update\n 2.internal\n")
 
-if mode == "Spotify":
-    print("test")
-    #spotify work
-elif mode == "internal update":
-    #print(internal())
-    playNextSong = input("Play nexr song? (y/n)")
-    if playNextSong == "y":
-        print(playSong())
-        print(continueWatching(songDuration))
-
-elif mode == "internal":
-    #mode = internal()
-
-    internal = 1
-    playNextSong = input("Play next song? (y/n)")
-    if playNextSong == "y":
-        print(playSong())
-        print(continueWatching(songDuration))
     
+
+
+        
     
         
             
